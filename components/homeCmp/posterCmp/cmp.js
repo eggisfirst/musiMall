783abed2943c7b64625a521d00c9aea46698d51f
xiaowei <578850593@qq.com>
2019-07-27 08:42:27 +0800

    优化

--- components/homeCmp/posterCmp/cmp.js
+++ components/homeCmp/posterCmp/cmp.js
@@ -179,13 +179,22 @@ Component({
             filePath: res.tempFilePath,
             success(res) { 
               that.savePoster()
-              // wx.switchTab({
-              //   url: '/pages/home/home'
+              // wx.showToast({
+              //   title: '保存成功',
+              //   icon: 'none',
+              //   duration: 2000
               // })
-              wx.showToast({
-                title: '保存成功',
-                icon: 'none',
-                duration: 2000
+              wx.showModal({
+                title: '提示',
+                content: '图片已保存',
+                showCancel: false,
+                success (res) {
+                  if (res.confirm) {
+                    console.log('用户点击确定')
+                  } else if (res.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
               })
             
             },
